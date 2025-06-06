d155e8ffdaa284ffd1186516995526ec4045d19f
Sergi Solvez <sssolvez@gmail.com>
2025-06-06 18:25:17 +0900

    add json fields for Token model

--- models/token.go
+++ models/token.go
@@ -5,17 +5,17 @@ import (
 )
 
 type Token struct {
-	ID                   int64     `gorm:"primaryKey;autoIncrement"`
-	CreatedAt            time.Time `gorm:"autoCreateTime"`
-	ContractAddress      string
-	UserAddress          string
-	LiquidityPoolAddress string
-	Name                 string
-	Symbol               string
-	InitialSupply        int64
-	DiscordURL           string
-	TelegramURL          string
-	XURL                 string
+	ID                   int64     `gorm:"primaryKey;autoIncrement" json:"id"`
+	CreatedAt            time.Time `gorm:"autoCreateTime" json:"created_at"`
+	ContractAddress      string    `json:"contract_address"`
+	UserAddress          string    `json:"user_address"`
+	LiquidityPoolAddress string    `json:"liquidity_pool_address"`
+	Name                 string    `json:"name"`
+	Symbol               string    `json:"symbol"`
+	InitialSupply        int64     `json:"initial_supply"`
+	DiscordURL           string    `json:"discord_url"`
+	TelegramURL          string    `json:"telegram_url"`
+	XURL                 string    `json:"x_url"`
 }
 
 type TokenInput struct {
